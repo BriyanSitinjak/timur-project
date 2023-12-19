3a82702dae8a268dcf0112561977570c45639f87
unknown <Imanbriyan@gmail.com>
2023-12-19 23:49:08 +0700

    Change the structure component

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-export default function ContactUs() {
+const ContactUs = () => {
   return (
-    <div className='d-flex align-middle justify-center w-screen h-screen'>
-      <div>
-        <p>Contact Us</p>
-      </div>
-    </div>
+    <div>ContactUs</div>
   )
 }
+
+export default ContactUs
